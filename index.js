c3a11d9d87ae9bd7bb48647a8208a00e298e3914
Madaga95 <74613897+Madaga95@users.noreply.github.com>
2022-09-08 16:23:50 +0200

    Click event

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //const back = (document.querySelector("body").style.background = "moccasin");
 
 // Click event
-
+const containers = document.querySelector(".container");
 const btn1 = document.getElementById("btn-1");
 const btn2 = document.getElementById("btn-2");
 const response = document.querySelector("p");
@@ -22,4 +22,16 @@ const mousemove = document.querySelector(".moussemove");
 window.addEventListener("mousemove", (e) => {
   mousemove.style.left = e.pageX + "px";
   mousemove.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mouseup", () => {
+  mousemove.style.border = "2px solid teal";
+});
+
+containers.addEventListener("mouseenter", () => {
+  containers.style.background = "rgba(0,0,0,0.4)";
+});
+
+containers.addEventListener("mouseout", () => {
+  containers.style.background = "moccasin";
 });
